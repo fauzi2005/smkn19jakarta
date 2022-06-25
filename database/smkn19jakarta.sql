400d12a5b11f8a2b74a3f54e4eb4f3c12c4f9589
Fauzi Maulana Habibi <fauzimaulanahabibi@gmail.com>
2022-06-26 01:32:09 +0700

    create Jurusan and Mapel

--- database/smkn19jakarta.sql
+++ database/smkn19jakarta.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 25 Jun 2022 pada 16.45
--- Versi server: 10.4.17-MariaDB
--- Versi PHP: 8.0.2
+-- Generation Time: Jun 25, 2022 at 08:30 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_guru`
+-- Table structure for table `jurusan_siswa`
+--
+
+CREATE TABLE `jurusan_siswa` (
+  `id_jurusan` int(11) NOT NULL,
+  `nama_jurusan` varchar(50) NOT NULL,
+  `kode_jurusan` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabelsiswa`
+--
+
+CREATE TABLE `tabelsiswa` (
+  `nis` varchar(255) NOT NULL,
+  `nisn` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `ttl` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `agama` varchar(255) NOT NULL,
+  `jurusan` varchar(255) NOT NULL,
+  `kelas` varchar(255) NOT NULL,
+  `no_telp` varchar(255) NOT NULL,
+  `email` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_guru`
 --
 
 CREATE TABLE `tb_guru` (
@@ -40,17 +72,58 @@ CREATE TABLE `tb_guru` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_guru`
+-- Dumping data for table `tb_guru`
 --
 
 INSERT INTO `tb_guru` (`kode_guru`, `nip_guru`, `nama_guru`, `tempat_lahir_guru`, `tanggal_lahir_guru`, `jenis_kelamin_guru`, `mata_pelajaran_guru`, `no_hp_guru`, `alamat_guru`) VALUES
-('001', '209440234', 'rifwan', 'jakarta', '22-06-3003', 'LAKI-LAKI', 'mtk', '08223444393', 'bogor'),
-('123a', '123', 'Fauzi', 'La', '12121997', 'LAKI-LAKI', 'MTK', '08111111111111', 'Bogor');
+('123a', '123', 'Fauzi', 'La', '12121997', 'LAKI-LAKI', 'MTK', '08111111111111', 'Bogor'),
+('KASJDHS', '91827393', 'ANWAR', 'TASIK', 'RAHASIA', 'LAKI-LAKI', 'PEMROGRAMAN', '012732392', 'JAKARTA SELATAN');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `tb_jurusan`
+--
+
+CREATE TABLE `tb_jurusan` (
+  `kode_jurusan` varchar(20) NOT NULL,
+  `nama_jurusan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_jurusan`
+--
+
+INSERT INTO `tb_jurusan` (`kode_jurusan`, `nama_jurusan`) VALUES
+('1', 'AP'),
+('5', 'Admin Perkantoran'),
+('7', 'asd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_mapel`
+--
+
+CREATE TABLE `tb_mapel` (
+  `kode_mapel` varchar(20) NOT NULL,
+  `nama_mapel` varchar(100) NOT NULL,
+  `kode_jurusan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_mapel`
+--
+
+INSERT INTO `tb_mapel` (`kode_mapel`, `nama_mapel`, `kode_jurusan`) VALUES
+('admin', 'admin', 'Admin Perkantoran'),
+('asa', 'ass', 'AP'),
+('asad', 'asdas', 'Admin Perkantoran');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -61,7 +134,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `level`) VALUES
@@ -73,24 +146,48 @@ INSERT INTO `users` (`id`, `username`, `password`, `level`) VALUES
 --
 
 --
--- Indeks untuk tabel `tb_guru`
+-- Indexes for table `jurusan_siswa`
 --
-ALTER TABLE `tb_guru`
-  ADD PRIMARY KEY (`kode_guru`);
+ALTER TABLE `jurusan_siswa`
+  ADD PRIMARY KEY (`id_jurusan`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `tabelsiswa`
+--
+ALTER TABLE `tabelsiswa`
+  ADD PRIMARY KEY (`nis`);
+
+--
+-- Indexes for table `tb_jurusan`
+--
+ALTER TABLE `tb_jurusan`
+  ADD PRIMARY KEY (`kode_jurusan`);
+
+--
+-- Indexes for table `tb_mapel`
+--
+ALTER TABLE `tb_mapel`
+  ADD PRIMARY KEY (`kode_mapel`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `jurusan_siswa`
+--
+ALTER TABLE `jurusan_siswa`
+  MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
